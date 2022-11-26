644eb33587f5b3120c17755b6c123dadfb5fd0e9
BAM-hub <bsharamin12@gmail.com>
2022-11-27 02:47:56 +0300

    server entry file

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { createServer } = require("http");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const { PrismaClient } = require("@prisma/client");
+require("dotenv").config();
+
+const app = express();
+const prisma = new PrismaClient();
+const server = createServer(app);
+
+async function main() {
+  if (prisma.$connect()) {
+    console.log("Connected to database");
+  }
+  app.use(express.json({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+
+  //   app.use(bodyParser.form);
+  app.use("/api/users", require("./routes/api/users"));
+  //   app.use("/api/auth", require("./routes/api/auth"));
+  app.use("/api/profile", require("./routes/api/profile"));
+
+  server.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
